702a979b0384f6e2d4e453863407be3f0207f26b
Prestonium <Preston.w.mcinelly@gmail.com>
2014-12-02 19:56:48 -0600

    Slower Scroll

--- dist/js/customjavascript.js
+++ dist/js/customjavascript.js
@@ -10,24 +10,28 @@ $(document).ready(function() {
 $(document).ready(function() {
 	// Click event to scroll to top
 	$(".me").click(function() {
-	    $('html, body').animate({
-	        scrollTop: $("#me").offset().top
-	    }, 2000);
+		$('html, body').animate({
+			scrollTop : $("#me").offset().top
+		}, 800);
 	});
 });
 $(document).ready(function() {
 	// Click event to scroll to top
 	$(".ach").click(function() {
-	    $('html, body').animate({
-	        scrollTop: $("#ach").offset().top
-	    }, 2000);
+		$('html, body').animate({
+			scrollTop : $("#ach").offset().top
+		}, 800);
 	});
 });
 $(document).ready(function() {
 	// Click event to scroll to top
 	$(".team").click(function() {
-	    $('html, body').animate({
-	        scrollTop: $("#team").offset().top
-	    }, 2000);
+		$('html, body').animate({
+			scrollTop : $("#team").offset().top
+		}, 800);
 	});
 });
+$('body').scrollspy({ target: '.navbar-example' })
+$('[data-spy="scroll"]').each(function() {
+	var $spy = $(this).scrollspy('refresh')
+})
